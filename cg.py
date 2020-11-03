ac583cd44d480fe76f67422a5204a34eb027bba7
Vladyslav Skrypchuk <51884701+Zmeykoo@users.noreply.github.com>
2020-11-03 22:51:45 +0200

    Add files via upload

--- cg.py
+++ cg.py
@@ -1,17 +1,17 @@
 import numpy as np
-matr = np.array([[2,3,4],[9,5,1],[8,9,1]])
+matr = np.array([[2,3,4],[9,5,1],[8,9,1],[2,0,1],[5,6,7]])
 
-Al = np.random.randint(1, size=(3,3))
-print(matr)
-"""
-for y in range(len(matr)):
-	for x in range(len(matr)):
-		matr[0][x]=0
-		print(matr)
-print(matr)"""
-matr=np.delete(matr, 2, 1)#убирає ряд
-print(matr)
-matr=np.delete(matr, 0, 0)#убирає с
+A = np.random.randint(10, size=(5,5))
+Al = np.random.randint(1, size=(5,5))
+print(A,'\n')
+def ob():
+	"""Метод обереної матриці"""
+	
 
+
+
+for x in range(len(A)):
+	A=np.delete(A, x, 1)#убирає column
+	A=np.delete(A, x, 0)#убирає row
+	print(A)
 #Al[x,y] = (round(np.linalg.det(matr)))
-print('\n\n',matr,'\n\n',Al)
---
+++ ch.py
@@ -0,0 +1,16 @@
+import numpy as np
+import copy as cp
+
+A = np.random.randint(10, size=(5,5))
+Cop=cp.copy(A)
+print(A)
+
+def ob(row, column):
+	"""mhj"""
+	Cop[x,y]=
+
+for x in range(len(A)):
+	for y in range(len(x)):
+		print(y)
+		
+		

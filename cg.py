85eaacc28b15ecb290f45fd5b8e3f5a074e97f6c
Vladyslav Skrypchuk <51884701+Zmeykoo@users.noreply.github.com>
2020-11-04 09:28:40 +0200

    Add files via upload

--- cg.py
+++ cg.py
@@ -1,24 +1,34 @@
 
 import copy as cp
 import numpy as np
-matr = np.array([[1,2,1],[3,4,1],[7,2,5]])
-n, m = 5, 5
-A = np.random.randint(10, size=(n,m))
-Al = np.random.randint(1, size=(n,m))
+matr = np.array([[2,-3,1],[2,1,-4],[6,-5,2]])
+det = round(np.linalg.det(matr))
+B = np.array([2,9,17])
+Al = cp.copy(matr)
 print('Origin matrix\n',matr,'\n')
 def ob(col,raw, matrix):
 	"""Метод обереної матриці"""
-	Num=matrix[raw][col]
-	matrix=np.delete(matrix, raw, 0)#убирає row
-	matrix=np.delete(matrix, col, 1)#убирає column
+	Num = matrix[raw][col]
+	matrix = np.delete(matrix, raw, 0)#убирає row
+	matrix = np.delete(matrix, col, 1)#убирає column
 	
 	det0=round(np.linalg.det(matrix))
+	if (col + raw)%2 != 0:
+		det0 *= -1
+		print(det0,'- +++++++ парне')
 	Al[raw][col]=det0
 	print(raw,col,'change. Num =',Num,'\nMatrix =\n',matrix)
-
-
 
 for x in range(len(matr)):
 	for y in range(len(matr)):
 		ob(y,x,matr)
-print(Al)
+print('Матриця алгебраїчних доповнень\n',Al)
+tran = np.transpose(Al)
+print('Транспонована матриця\n',tran)
+tryres = tran*B
+print('X =\n',tryres)
+result=[]
+for f in tryres:
+	result.append(sum(f)/det)
+
+print('Result =',result)

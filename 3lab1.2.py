b5ad7007a274dc79f78044e7aa044bfc097cd797
Vladyslav Skrypchuk <51884701+Zmeykoo@users.noreply.github.com>
2020-11-04 18:24:54 +0200

    Add files via upload

---
+++ 3lab1.2.py
@@ -0,0 +1,15 @@
+import numpy as np
+import copy as cp
+def det():
+	"""Searching det"""
+	print(round(np.linalg.det(a))) #easy method
+	
+
+
+
+a = np.array([[2,4,32],[24,5,87],[21,1,0]])
+b = np.array([[45,2,3],[67,0,7],[33,21,4]])
+
+
+print(a)
+det()

b794b478d3fc52c9fa013db1960abffd4c09ee16
Vladyslav Skrypchuk <51884701+Zmeykoo@users.noreply.github.com>
2020-11-05 01:30:22 +0200

    Add files via upload

--- 3lab1.2.py
+++ 3lab1.2.py
@@ -4,18 +4,23 @@ import copy as cp
 det=0
 def determ(matr,det):
 	"""Searching det"""
-	print('det =',round(np.linalg.det(b))) #easy method
-	det=0
+	def three(minmatr):
+		"""Пошук визначника 3x3"""
+		det = matr[0,0]*matr[1,1]*matr[2,2]+matr[0,1]*matr[1,2]*matr[2,0]+matr[0,2]*matr[1,0]*matr[2,1]-matr[0,2]*matr[1,1]*matr[2,0]-matr[0,0]*matr[1,2]*matr[2,1]-matr[0,1]*matr[1,0]*matr[2,2]
+		print(det)
+	print('Matrix\n',matr)
+	print('det =',round(np.linalg.det(matr))) #easy method
+
 	if len(matr) == 1:
 		det = mat[0,0]
 	elif len(matr) == 2:
 		print('byt')#det = matr[0,0]*matr[1,1]-matr[0,1]*matr[1,0]
 	elif len(matr) == 3:
-		det == matr[0,0]*matr[1,1]*matr[2,2]+matr[0,1]*matr[1,2]*matr[2,0]+matr[0,2]*matr[1,0]*matr[2,1]-matr[0,2]*matr[1,1]*matr[2,0]-matr[0,0]*matr[1,2]*matr[2,1]-matr[0,1]*matr[1,0]*matr[2,2]
-		print(matr)
-		print (matr[0,0],matr[1,1],matr[2,2],matr[0,1],matr[1,2],matr[2,0],matr[0,2]*matr[1,0],matr[2,1],matr[0,2],matr[1,1],matr[2,0],matr[0,0],matr[1,2],matr[2,1],matr[0,1],matr[1,0],matr[2,2])
+		three(matr)
 		print ('det =',det)
 	elif len(matr) >= 4:
+		
+		
 		print('')
 	else:
 		print('Здається твоя матриця неквадратна, друже...')
@@ -27,4 +32,4 @@ c = np.array([[1,2],[2,1]])
 
 print(len(c[:,:]),'\n')
 determ(b,det)
-print('det =',det)
+

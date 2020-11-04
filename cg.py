0c2928bb8bd8b10c16e2b1dc624f7e6279b52789
Vladyslav Skrypchuk <51884701+Zmeykoo@users.noreply.github.com>
2020-11-04 08:55:37 +0200

    Add files via upload

--- cg.py
+++ cg.py
@@ -1,17 +1,24 @@
+
+import copy as cp
 import numpy as np
-matr = np.array([[2,3,4],[9,5,1],[8,9,1],[2,0,1],[5,6,7]])
-
-A = np.random.randint(10, size=(5,5))
-Al = np.random.randint(1, size=(5,5))
-print(A,'\n')
-def ob():
+matr = np.array([[1,2,1],[3,4,1],[7,2,5]])
+n, m = 5, 5
+A = np.random.randint(10, size=(n,m))
+Al = np.random.randint(1, size=(n,m))
+print('Origin matrix\n',matr,'\n')
+def ob(col,raw, matrix):
 	"""Метод обереної матриці"""
+	Num=matrix[raw][col]
+	matrix=np.delete(matrix, raw, 0)#убирає row
+	matrix=np.delete(matrix, col, 1)#убирає column
 	
+	det0=round(np.linalg.det(matrix))
+	Al[raw][col]=det0
+	print(raw,col,'change. Num =',Num,'\nMatrix =\n',matrix)
 
 
 
-for x in range(len(A)):
-	A=np.delete(A, x, 1)#убирає column
-	A=np.delete(A, x, 0)#убирає row
-	print(A)
-#Al[x,y] = (round(np.linalg.det(matr)))
+for x in range(len(matr)):
+	for y in range(len(matr)):
+		ob(y,x,matr)
+print(Al)
